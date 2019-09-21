cdcc54978658f1a37925ee89479c617d0d16833f
unknown <Digital@DH-A8-19.dhbrasil.digitalhouse.com>
2019-09-21 10:09:02 -0300

    Drawable

---
+++ Projeto_IMC/app/src/main/java/com/example/projeto_imc/BoasVindas.java
@@ -0,0 +1,13 @@
+package com.example.projeto_imc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BoasVindas extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boas_vindas);
+    }
+}
